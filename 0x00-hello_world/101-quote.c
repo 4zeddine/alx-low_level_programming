9ddfe364501c4a2459373276f1712d25c60b21a7
4zeddine <azeddine.nader@gmail.com>
2023-10-05 07:46:01 -0700

    editing 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
 */
 int main(void)
 {
-char sentence[] = "and that piece of art is useful\"\
- - Dora Korpar, 2015-10-19\n";
+char sentence[] = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n";
 int sentence_lenght = 59;
 write(1, sentence, sentence_lenght);
 return (1);
