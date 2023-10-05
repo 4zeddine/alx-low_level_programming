87a9398b912435121905a8ecc2c2d1c1902c4d9a
4zeddine <azeddine.nader@gmail.com>
2023-10-05 06:05:24 -0700

    editing 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main()
+/**
+*main - the main function in our program
+*Return: return 0
+*/
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
