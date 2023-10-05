4891a74191a75130e2c04181409fad2f79999113
4zeddine <azeddine.nader@gmail.com>
2023-10-05 07:42:22 -0700

    adding 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+*main - the main function in our program
+*Return: return 0
+*/
+int main(void)
+{
+char sentence[] = "and that piece of art is useful\"\
+- Dora Korpar, 2015-10-19\n";
+int sentence_lenght = 58;
+write(1, sentence, sentence_lenght);
+return (1);
+}
