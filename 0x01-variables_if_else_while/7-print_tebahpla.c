b98d52c22695fc8a71eafb627506d591be3cf125
4zeddine <azeddine.nader@gmail.com>
2023-10-06 12:07:02 -0700

    adding 7-print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+*main - the main function in our program
+*Return: return 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int a = 122;
+while (a > 96)
+{
+putchar(a);
+a--;
+}
+putchar('\n');
+/* your code goes there */
+return (0);
+}
