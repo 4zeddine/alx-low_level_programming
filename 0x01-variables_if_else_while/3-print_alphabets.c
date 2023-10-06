4fa7082e80b7eb841eb4f2c823b6fca13246a344
4zeddine <azeddine.nader@gmail.com>
2023-10-06 10:56:00 -0700

    adding 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+*main - the main function in our program
+*Return: return 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int a = 97;
+while (a < 123)
+{
+putchar(a);
+a++;
+}
+a = 65;
+while (a < 91)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+/* your code goes there */
+return (0);
+}
