de3dd80b7a8b9c3f4d06fa11dd069cb45bdc5b2d
4zeddine <azeddine.nader@gmail.com>
2023-10-06 12:11:24 -0700

    adding 8-print_base16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+*main - the main function in our program
+*Return: return 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int a = 48;
+while (a < 58)
+{
+putchar(a);
+a++;
+}
+a = 97;
+while (a < 103)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+/* your code goes there */
+return (0);
+}
