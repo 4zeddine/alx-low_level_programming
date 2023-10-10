9ec2c283786521bde7ff11889cb367caa69253a6
4zeddine <azeddine.nader@gmail.com>
2023-10-10 07:49:48 -0700

    adding 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - the main function in our program
+*Return: return 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int undr = '_', p = 'p', u = 'u', t = 't', c = 'c', h = 'h', a = 'a', r = 'r';
+_putchar(undr);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+/* your code goes there */
+return (0);
+}
