64ef0686618949b073442d235b70f7fb23eeeb01
4zeddine <azeddine.nader@gmail.com>
2023-10-10 12:08:32 -0700

    adding files

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ *
+ * Return: no return
+ */
+
+void times_table(void)
+{
+int i, j, n;
+for (i = 0 ; i < 10 ; i++)
+{
+for(j = 0 ; j < 10 ; j++)
+{
+n = i * j;
+if (n / 10 == 0)
+{
+_putchar(n + 48);
+}
+else
+{
+_putchar((n / 10) + 48);
+_putchar((n % 10) + 48);
+}
+if (j != 9)
+{
+_putchar(44);
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
