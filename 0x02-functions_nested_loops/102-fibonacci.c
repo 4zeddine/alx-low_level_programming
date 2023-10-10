60baf17e1faffe90fb8c3dde59eb4efe9a26dfe6
4zeddine <azeddine.nader@gmail.com>
2023-10-10 15:28:54 -0700

    adding files

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+
+/**
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	long int n1, n2, fb;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (i = 0; i < 48; i++)
+	{
+		fb = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fb;
+	}
+	printf("\n");
+	return (0);
+}
