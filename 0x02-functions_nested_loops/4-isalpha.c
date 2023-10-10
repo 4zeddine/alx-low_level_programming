0b8f286577c74d1f4663436d501b780635fec607
4zeddine <azeddine.nader@gmail.com>
2023-10-10 10:07:32 -0700

    adding files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Shows 1 if the input is a
+ * letter Another cases, shows 0
+ *
+ * Return: 1 for letters. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+if (c > 64 && c < 91 || c > 96 && c < 123)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
