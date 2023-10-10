1010620cdfc1519626eef7a999dcfe4353fe11f8
4zeddine <azeddine.nader@gmail.com>
2023-10-10 15:18:55 -0700

    adding files

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
