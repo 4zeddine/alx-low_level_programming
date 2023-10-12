b3a6848b0b4fbe9e882c5daf8fab9da8e946e8b1
4zeddine <azeddine.nader@gmail.com>
2023-10-12 04:08:23 -0700

    editing file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,18 @@
 /**
  * _isupper - check for uppercase characters
  *
- * Return: Always 0.
+ * @c: the letter to be checked
+ *
+ * Return: 1 if c is uppercase or 0 otherwise.
  */
 
 int _isupper(int c)
 {
-if(c > 64 && c < 91)
+if (c > 64 && c < 91)
 {
 return (1);
 }
-else if (c > 96 && c < 123)
+else
 {
 return (0);
 }
