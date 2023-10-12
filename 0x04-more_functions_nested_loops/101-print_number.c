21130c8ab2c31070460b9128f2901bfc0a4ae9b5
4zeddine <azeddine.nader@gmail.com>
2023-10-12 13:39:51 -0700

    adding files

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+unsigned int num, d, count;
+if (n < 0)
+{
+putchar(45);
+num = n * -1;
+}
+else
+{
+num = n;
+}
+d = num;
+count = 1;
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+for (; count >= 1; count /= 10)
+{
+putchar(((num / count) % 10) + 48);
+}
+}
