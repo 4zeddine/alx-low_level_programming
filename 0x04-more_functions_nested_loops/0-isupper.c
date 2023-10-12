0e5be1738842c56bf119a7bbe4361287205af69d
4zeddine <azeddine.nader@gmail.com>
2023-10-12 04:02:35 -0700

    adding files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase characters
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+if(c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+projects for 0x04-more_functions_nested_loops 
