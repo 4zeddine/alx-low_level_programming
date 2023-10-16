7a940162d9d64bd4d7112f498b6d80f20b3a1c88
4zeddine <azeddine.nader@gmail.com>
2023-10-16 06:41:56 -0700

    adding files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value to the pointed variable
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Projects for 0x05-pointers_arrays_strings  
