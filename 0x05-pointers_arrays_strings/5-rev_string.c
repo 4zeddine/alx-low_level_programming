dafe6692e108738ac62a0da040883ac604a9a045
4zeddine <azeddine.nader@gmail.com>
2023-10-16 11:23:34 -0700

    adding files

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,16 @@
 void rev_string(char *s)
 {
 int i = 0, size1 = 0, size2;
-char tmp;
 while (s[i] != '\0')
 {
-size1++;
+size++;
 i++;
 }
-size2 = size - 1;
-for (i = 0 ; i < size1 ; i++)
+size2 = size1 - 1;
+for (i = 0 ; i < size1 / 2 ; i++)
 {
 tmp = s[i];
-s[i] = s[size2 -1];
+s[i] = s[size2];
 s[size2] = tmp;
 size2--;
 }
