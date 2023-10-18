45a0fa2303df69b7df9e8aa2f2e2b529af19c86f
4zeddine <azeddine.nader@gmail.com>
2023-10-18 14:37:19 -0700

    adding files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input of first string
+ * @src: input of second string
+ * Return: pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j = 0, k = 0, size1 = 0, size2 = 0 ;
+char *ptr;
+while(*(dest + i) != '\0')
+{
+size1++;
+i++;
+}
+while(*(src + j) != '\0')
+{
+size2++;
+j++;
+}
+for (k = size1 ;  k <= (size1 + size2 + 1) ; k++)
+{
+if (k <= size1 + size2)
+{
+*(dest + k) = *(src + k - size1);
+}
+else
+{
+*(dest + k) = '\0'; 
+}
+}
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+projects for 0x06-pointers_arrays_strings 
