8058847e2f2196ff338a99d30c19aab1b9d4e4aa
4zeddine <azeddine.nader@gmail.com>
2023-10-18 15:21:55 -0700

    editing file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0, j = 0, k = 0, size1 = 0, size2 = 0 ;
-while(*(dest + i) != '\0')
+int i = 0, j = 0, k = 0, size1 = 0, size2 = 0;
+while (*(dest + i) != '\0')
 {
 size1++;
 i++;
 }
-while(*(src + j) != '\0')
+while (*(src + j) != '\0')
 {
 size2++;
 j++;
