6f3361f917fa2aa05947039e97d1735566549ba8
4zeddine <azeddine.nader@gmail.com>
2023-10-19 04:09:26 -0700

    adding files

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: input of first string
+ * @src: input of second string
+ * @n: input of bytes
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+for (i = 0 ; i < n && *(src + i) != '\0' ; i++)
+{
+*(dest + i) = *(src + i);
+}
+while (i < n)
+{
+*(dest + i) = '\0';
+i++;
+}
+return (dest);
+}
