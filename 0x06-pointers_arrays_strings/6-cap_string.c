1cdbc28a17a98444ae3b5825bf96af20945ab7b9
4zeddine <azeddine.nader@gmail.com>
2023-10-19 09:05:53 -0700

    adding files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,26 @@
 
 char *cap_string(char *str)
 {
-int i = 0;
-while (*(str + i) != '\0')
+int count = 0, i;
+int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+if (*(str + count) >= 97 && *(str + count) <= 122)
 {
-if (*(str + i) == 32 || *(str + i) == 33 || *(str + i) == 34
-|| *(str + i) == 40 || *(str + i) == '\n' || *(str + i) == 10
-|| *(str + i) == 41 || *(str + i) == 44 || *(str + i) == 46
-|| *(str + i) == 59 || *(str + i) == 9
-|| *(str + i) == 63 || *(str + i) == 123 || *(str + i) == 125)
+*(str + count) = *(str + count) - 32;
+}
+count++;
+while (*(str + count) != '\0')
 {
-if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
+for (i = 0; i < 13; i++)
 {
-*(str + i + 1) = *(str + i + 1) - 32;
+if (*(str + count) == sep_words[i])
+{
+if ((*(str + (count + 1)) >= 97) && (*(str + (count + 1)) <= 122))
+*(str + (count + 1)) = *(str + (count + 1)) - 32;
 break;
 }
 }
-i++;
+count++;
 }
 return (str);
 }
