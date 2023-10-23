7fcf163c615f80ff2145d46c23e319860b3d8648
4zeddine <azeddine.nader@gmail.com>
2023-10-23 06:46:39 -0700

    adding files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcopy - copies the first n bytes of the memory area\
+ * from src to the memory area dest
+ * @dest: input of pointer to memory area
+ * @src: input of pointer to memory area
+ * @n: input of number of bytes
+ *
+ * Return: a pointer to memory area dest.
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0 ; i < n ; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
