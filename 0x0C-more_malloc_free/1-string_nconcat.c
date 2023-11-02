1825ac00ef3b97f95d16082b46c4dd0c5a2a83fb
4zeddine <azeddine.nader@gmail.com>
2023-11-02 11:31:31 -0700

    adding file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,29 +14,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *p;
-unsigned int i = 0, j = 0, m, k;
+unsigned int i, j, l, k;
+
 if (s1 == NULL)
 s1 = "";
+
 if (s2 == NULL)
 s2 = "";
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-j++;
-if (n >= j)
-m = i + j;
-else
-m = i + n;
-p = malloc(m + 1);
+
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
+
+if (n > j)
+n = j;
+
+l = i + n;
+p = malloc(l + 1);
+
 if (p == NULL)
 return (NULL);
-for (k = 0; k < m; k++)
-{
-if (k <= i)
-p[k] = s1[i];
+for (k = 0; k < l; k++)
+if (k < i)
+p[k] = s1[k];
 else
 p[k] = s2[k - i];
-}
+
 p[k] = '\0';
+
 return (p);
 }
