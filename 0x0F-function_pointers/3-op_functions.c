1db810a3647064d1e413caa5141db61e3305d64a
4zeddine <azeddine.nader@gmail.com>
2023-11-08 12:54:51 -0800

    adding files

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,6 @@
  *
  * Return: add.
  */
-
 int op_add(int a, int b)
 {
 return (a + b);
@@ -20,7 +19,6 @@ return (a + b);
  *
  * Return: difference.
  */
-
 int op_sub(int a, int b)
 {
 return (a - b);
@@ -33,7 +31,6 @@ return (a - b);
  *
  * Return: multiplication.
  */
-
 int op_mul(int a, int b)
 {
 return (a * b);
@@ -46,7 +43,6 @@ return (a * b);
  *
  * Return: division.
  */
-
 int op_div(int a, int b)
 {
 if (b == 0)
@@ -64,8 +60,12 @@ return (a / b);
  *
  * Return: remainder of the division.
  */
-
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
