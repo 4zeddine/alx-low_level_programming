d8dfea6dd33ca140e764dfe172d2c8dfc0204682
4zeddine <azeddine.nader@gmail.com>
2023-11-08 12:34:17 -0800

    adding files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int a, b, (*opp)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+operation = get_op_func(argv[2]);
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+
+if (!opp)
+{
+printf("Error\n");
+exit(99);
+}
+
+if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+{
+printf("Error\n");
+exit(100);
+}
+
+printf("%d\n", opp(a, b));
+return (0);
+}
