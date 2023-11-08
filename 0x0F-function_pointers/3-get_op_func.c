d4b5f9c152cc51821b729c91ecf21fdc917d61a4
4zeddine <azeddine.nader@gmail.com>
2023-11-08 13:15:22 -0800

    adding files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,11 +19,12 @@ op_t ops[] = {
 {NULL, NULL}
 };
 int i = 0;
-while (i < 5)
+while (i < 10)
 {
-if (s && s[0] == ops[i].op[0] && !s[1])
-return (ops.[i].f);
+if (s[0] == ops->op[i])
+break;
 i++;
 }
-return (NULL);
+
+return (ops[i / 2].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,30 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b, (*opp)(int, int);
-
+int a, b;
+int (*opp)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
 
-opp = get_op_func(argv[2]);
-
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-
-if (!opp)
+if (argv[2][1])
 {
 printf("Error\n");
 exit(99);
 }
 
-if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+opp = get_op_func(argv[2]);
+
+if (opp == NULL)
 {
 printf("Error\n");
-exit(100);
+exit(99);
 }
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
 
 printf("%d\n", opp(a, b));
 return (0);
