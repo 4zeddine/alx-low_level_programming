0b05adf2764f390e9df29a68d5dc1048e9f19789
4zeddine <azeddine.nader@gmail.com>
2024-02-02 10:37:19 -0800

    first commit

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: the hash table
+ * Return: no return
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *node, *tmp;
+hash_table_t *head;
+
+head = ht;
+
+for (i = 0; i < ht->size; i++)
+{
+	if (ht->array[i])
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = nod->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+}
+free(head->array);
+free(head);
+}
