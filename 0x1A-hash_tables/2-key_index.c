0e44f257bdbc343e3be88fc8b5cd1cc9cbaefd02
4zeddine <azeddine.nader@gmail.com>
2024-02-02 08:10:04 -0800

    edit commit

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of the key passed
+ *
+ * @key: key input
+ * @size: size of the hash tables
+ * Return: the index of the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+unsigned long int id;
+
+if (!key || !size)
+	return (0);
+
+id = hash_gjb2(key) % size;
+
+return (id);
+}
